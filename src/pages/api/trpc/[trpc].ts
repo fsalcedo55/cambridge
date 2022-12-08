f93456cfc6b1c2e3b386e5b22a3ac86bca93e63f
fsalcedo55 <fsalcedo55@hotmail.com>
2022-12-08 14:37:55 -0500

    Import createContext to tRPC router definition.

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,8 @@
 import { appRouter } from "src/server/routers/_app"
 import * as trpcNext from "@trpc/server/adapters/next"
+import { createContext } from "@src/server/context"
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => ({}),
+  createContext,
 })
