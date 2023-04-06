c686e286c5ef8208fc1dd7201500ca78bb6cf30e
fsalcedo55 <fsalcedo55@hotmail.com>
2023-04-06 11:39:42 -0400

    Add url to next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "placeimg.com", "images.unsplash.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "placeimg.com",
+      "images.unsplash.com",
+      "cdn-icons-png.flaticon.com",
+    ],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
