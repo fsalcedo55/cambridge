f38b94411733fc48692ae534b4759ec65e8710dd
fsalcedo55 <fsalcedo55@hotmail.com>
2023-10-22 11:00:27 -0400

    feat: refactor sidebar code, add external tools section, add loading component

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,77 +1,158 @@
-import Link from "next/link"
+import {
+  adminNavigation,
+  adminNavigationExternal,
+  teacherNavigation,
+  teacherNavigationExternal,
+} from "@src/constants/navigation"
 import { useSession } from "next-auth/react"
-import { AiFillHome } from "react-icons/ai"
+import Link from "next/link"
 import { useRouter } from "next/router"
-import { HiUsers } from "react-icons/hi"
-import { FaChild } from "react-icons/fa"
+import { TbExternalLink } from "react-icons/tb"
+import Divider from "../ui/Divider"
+import { FC } from "react"
 import LoadingSkeleton from "../ui/loadingSkeleton"
 
-export default function Sidebar() {
-  const { data: session, status } = useSession()
-  const router = useRouter()
-  const loading = status === "loading"
-
-  const adminMenu = [
-    { icon: <AiFillHome />, name: "Admin Dashboard", href: "/admin/dashboard" },
-    { icon: <HiUsers />, name: "Users", href: "/admin/users" },
-    { icon: <FaChild />, name: "Students", href: "/admin/students" },
-  ]
-
-  const teacherMenu = [
-    // {
-    //   icon: <AiFillHome />,
-    //   name: "Teacher Dashboard",
-    //   href: "/teacher/dashboard",
-    // },
-    { icon: <FaChild />, name: "Students", href: "/teacher/students" },
-  ]
-
-  type Menu = {
-    icon: JSX.Element
+interface NavItemProps {
+  item: {
     name: string
     href: string
-  }[]
-
-  const menuMap = (arr: Menu) => {
-    return arr.map((item: any, idx: number) => (
-      <li key={idx}>
-        <Link href={item.href}>
-          {router.pathname.includes(item.href) ? (
-            <a className="active bg-primary-100 text-primary-900 hover:bg-primary-200">
-              <span>{item.icon}</span>
-              <span>{item.name}</span>
-            </a>
-          ) : (
-            <a className="hover:bg-primary-200 text-primary-900">
-              <span>{item.icon}</span>
-              <span>{item.name}</span>
-            </a>
-          )}
-        </Link>
-      </li>
-    ))
+    icon: any
   }
+  isExternal?: boolean
+  active: boolean
+}
 
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ")
+}
+
+const NavItem: FC<NavItemProps> = ({ item, isExternal = false, active }) => {
+  const baseClasses = classNames(
+    active
+      ? "bg-primary-600 text-primary-50"
+      : "text-primary-600 hover:bg-neutral-100 hover:text-primary-700",
+    "group flex items-center px-2 py-2 text-md rounded-full cursor-pointer"
+  )
+
+  const content = (
+    <div className={baseClasses}>
+      <item.icon className="mr-3 text-2xl" aria-hidden="true" />
+      {isExternal ? (
+        <div className="flex items-center gap-2">
+          {item.name}
+          <span className="opacity-50">
+            <TbExternalLink />
+          </span>
+        </div>
+      ) : (
+        <div className="font-bold ">{item.name}</div>
+      )}
+    </div>
+  )
+
+  return isExternal ? (
+    <a
+      key={item.name}
+      href={item.href}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      {content}
+    </a>
+  ) : (
+    <Link key={item.name} href={item.href}>
+      {content}
+    </Link>
+  )
+}
+
+interface NavigationSectionProps {
+  items: NavItemProps["item"][]
+  isExternal?: boolean
+  router: any
+  status: string
+}
+
+const NavigationSection: FC<NavigationSectionProps> = ({
+  items,
+  isExternal = false,
+  router,
+  status,
+}) => {
   return (
     <div>
-      <div className="fixed flex-none py-4 h-96 top-16 drawer-side">
-        <ul className="w-64 h-64 p-4 bg-white shadow rounded-xl menu text-primary-800">
-          {!session && loading ? (
-            <div className="flex flex-col gap-4">
-              <LoadingSkeleton />
-              <LoadingSkeleton />
-            </div>
-          ) : (
-            <div className="font-bold text-md">
-              {session?.role == "admin" ? (
-                <>{menuMap(adminMenu)}</>
-              ) : (
-                <>{menuMap(teacherMenu)}</>
-              )}
-            </div>
+      {items.map((item) => (
+        <NavItem
+          key={item.name}
+          item={item}
+          isExternal={isExternal}
+          active={router.pathname.includes(item.href)}
+        />
+      ))}
+    </div>
+  )
+}
+
+const ExternalToolsDivider = () => (
+  <div className="mb-4">
+    <Divider label="External Tools" />
+  </div>
+)
+
+const Sidebar: FC = () => {
+  const router = useRouter()
+  const { data: session, status } = useSession()
+
+  if (status == "loading") return <LoadingSkeleton />
+  console.log("status: ", status)
+
+  return (
+    <div
+      className={`flex flex-col ml-8 bg-white shadow ${"height-at-top"} rounded-xl overflow-auto`}
+    >
+      <div className="flex flex-col flex-1 mt-5">
+        <nav className="flex flex-col justify-between flex-1 px-2 pb-4 space-y-1">
+          {session?.role === "admin" && (
+            <>
+              <NavigationSection
+                items={adminNavigation}
+                router={router}
+                status={status}
+              />
+              <div>
+                <ExternalToolsDivider />
+                <NavigationSection
+                  items={adminNavigationExternal}
+                  router={router}
+                  isExternal={true}
+                  status={status}
+                />
+              </div>
+            </>
           )}
-        </ul>
+          {session?.role === "teacher" && (
+            <>
+              <NavigationSection
+                items={teacherNavigation}
+                router={router}
+                status={status}
+              />
+
+              <div>
+                <ExternalToolsDivider />
+                <NavigationSection
+                  items={teacherNavigationExternal}
+                  router={router}
+                  isExternal={true}
+                  status={status}
+                />
+              </div>
+            </>
+          )}
+        </nav>
       </div>
     </div>
   )
 }
+
+export default Sidebar
